4f0fe2df4c3ee3dc3988a75d94b3ac3a1840a9ce
Hamish Croser <h.croser1@gmail.com>
2024-06-03 23:12:49 +1000

    Added color and width gradients

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,79 @@
-const STROKE_WIDTH = 1;
-const BRANCH_COLOR = "#AD343E";
-const APP_BACKGROUND_COLOR = "#000000";
+const BRANCH_COLOR_START = "#AD343E";
+const BRANCH_COLOR_END = "#42D9C8";
+const BRANCH_WIDTH_START = 0.5;
+const BRANCH_WIDTH_END = 1.0;
+const APP_BACKGROUND_COLOR = "#121016";
+
+function buildWidthMap(startWidth, endWidth, numLayers) {
+    const widthDiff = endWidth - startWidth;
+
+    const widthMap = new Map();
+    for (let currLayer = 0; currLayer < numLayers; currLayer++) {
+        let width = (widthDiff * (currLayer / numLayers)) + startWidth;
+        widthMap.set(currLayer, width);
+    }
+
+    return widthMap;
+}
+
+function buildColorMap(startColor, endColor, numLayers) {
+    const startRGB = hexToRGB(startColor);
+    const endRGB = hexToRGB(endColor);
+    let ratio = 1.0;
+
+    const colorMap = new Map();
+    for (let currLayer = 0; currLayer < numLayers; currLayer++) {
+        const interpolatedColorRGB = [];
+        for (let i = 0; i < startRGB.length; i++) {
+            let interpolatedInt = Math.round(((endRGB[i] - startRGB[i]) * ratio) + startRGB[i]);
+            interpolatedColorRGB.push(interpolatedInt);
+        }
+        colorMap.set(currLayer, RGBtoHex(interpolatedColorRGB));
+
+        ratio *= 0.5;
+    }
+
+    return colorMap;
+}
+
+function hexToRGB(colorHex) {
+    const rgbColor = [];
+    for (let i = 1; i < 7; i += 2) {
+        rgbColor.push(parseInt(colorHex.substring(i, i+2), 16));
+    }
+
+    return rgbColor;
+}
+
+function RGBtoHex(colorRGB) {
+    let colorHex = "#";
+    for (let component of colorRGB) {
+        colorHex += Number(component).toString(16).padStart(2, '0');
+    }
+    return colorHex;
+}
 
 function toRadians(degrees) {
     return degrees * (Math.PI / 180);
 }
 
-function generateTree(graphics, branchLength, angleOffsetConstant, currLayer, angleOffset, addedOffset, nodeX, nodeY) {
+function generateTree(fractalStage, colorMap, widthMap, branchLength, angleOffsetConstant, currLayer, angleOffset, addedOffset, nodeX, nodeY) {
     if (currLayer === 0) {
         return;
     }
     let newLayer = currLayer - 1;
+    // console.log(`Start: ${BRANCH_START_COLOR}, end: ${BRANCH_END_COLOR}, currLayer: ${currLayer}, totalLayers: ${totalLayers}`);
 
     let leftOffset = angleOffset + angleOffsetConstant;
     let rightOffset = angleOffset - angleOffsetConstant;
     for (let offset of [leftOffset, rightOffset]) {
         let newX = nodeX + branchLength * Math.sin(offset + addedOffset);
         let newY = nodeY + branchLength * Math.cos(offset + addedOffset);
-        graphics.moveTo(nodeX, nodeY);
-        graphics.lineTo(newX, newY);
-        graphics.stroke({ width: STROKE_WIDTH, color: BRANCH_COLOR });
+        fractalStage.moveTo(nodeX, nodeY);
+        fractalStage.lineTo(newX, newY);
+        fractalStage.stroke({ width: widthMap.get(newLayer), color: colorMap.get(newLayer) });
 
-        generateTree(graphics, branchLength, angleOffsetConstant, newLayer, offset, addedOffset, newX, newY);
+        generateTree(fractalStage, colorMap, widthMap, branchLength, angleOffsetConstant, newLayer, offset, addedOffset, newX, newY);
     }
 }
 
@@ -32,11 +84,14 @@ function generateTreeFromInputs(fractalStage, midpoint) {
     let degreesOffset = parseInt(document.getElementById("angle-input").value);
     let numRoots = parseInt(document.getElementById("num-trees-input").value);
 
+    let colorMap = buildColorMap(BRANCH_COLOR_START, BRANCH_COLOR_END, numLayers);
+    let widthMap = buildWidthMap(BRANCH_WIDTH_START, BRANCH_WIDTH_END, numLayers);
+
     branchLength = -branchLength;
     let addedDegrees = 0;
     for (let root = 1; root <= numRoots; root++) {
         addedDegrees = (360 / numRoots) * root;
-        generateTree(fractalStage, branchLength, toRadians(degreesOffset), numLayers, 0, toRadians(addedDegrees), midpoint[0], midpoint[1]);
+        generateTree(fractalStage, colorMap, widthMap, branchLength, toRadians(degreesOffset), numLayers, 0, toRadians(addedDegrees), midpoint[0], midpoint[1]);
     }
 }
 
